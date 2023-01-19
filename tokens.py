67ac84c254a6a4c56d75c2f2237f5deceddb6934
amJenish <121776923+amJenish@users.noreply.github.com>
2023-01-19 04:06:28 -0500

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -3,5 +3,3 @@ consumerSecret = 'UZTVBqKl1FvxEMGJp3Bj6e77l5SNDuPHrViHhzkLKJQC8rtEfA'
 accessToken = '1615092980775264258-gPsFLCIhMbrCfavWvwZaoazMsuSvND'
 accessSecret = 'Ycs8CEV6glRjbA5wFpwPDv1iisIy5YJsPewzTocrz14W6'
 bearerToken = "AAAAAAAAAAAAAAAAAAAAADULlQEAAAAAc5pN2Y9Fk9hPWXO%2BzvYliMVCqms%3DZnFkvWBPVhWSzdFy3KruJySwiKK6KLUnymDiffAJpYbjxQvwsg"
-
-openAI_API = "sk-J5SAktoXaEECas1K4PxBT3BlbkFJ5aoXNteSt7NNo9uZqBF6"
